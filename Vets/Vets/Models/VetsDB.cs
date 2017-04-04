72a6024a0db196fd520d3c0aac38be05002a51ef
Okica <aluno19422@ipt.pt>
2017-04-04 12:28:08 +0100

    criação inicial

---
+++ Vets/Vets/Models/VetsDB.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Vets.Models
+{
+    public class VetsDB : DbContext
+    {
+        //representar a base de dados
+        //descrevendo as tabelas que lá estão contidas
+
+        //representar o 'construtor' desta classe
+        //identifica onde se encontra a Base de Dados
+        public VetsDB() : base ("VetsDBConnection"){ }
+
+        //descrever as 'tabelas' que estao na BD
+        public virtual DbSet<Donos> Donos { get; set; }
+
+
+    }
+}
